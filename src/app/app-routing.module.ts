b71dd90ed42f31e65c8f41110081478dc31ef194
--replace-all <leonunesleo2021@gmail.com>
2022-11-25 14:52:58 -0300

    App02

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,6 +11,14 @@ const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full'
   },
+  {
+    path: 'games',
+    loadChildren: () => import('./pages/games/games.module').then( m => m.GamesPageModule)
+  },
+  {
+    path: 'game-deatils',
+    loadChildren: () => import('./pages/game-details/game-details.module').then( m => m.GameDetailsPageModule)
+  },
 ];
 
 @NgModule({
